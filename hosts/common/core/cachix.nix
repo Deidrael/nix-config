2af5f486cae6356b08a2d746295d10f7acbebd0c
Adam Grissom <14314946+Deidrael@users.noreply.github.com>
2025-08-10 16:44:25 -0500

    adding nix-community cachix

--- hosts/common/core/cachix.nix
+++ hosts/common/core/cachix.nix
@@ -4,9 +4,11 @@
     substituters = [
       "https://deidrael.cachix.org"
       "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
     ];
     trusted-public-keys = [
       "deidrael.cachix.org-1:73m+qt2qGNI8fhTuM0qwDM3QQM6WdGLxELwucd3+JdA="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 }
