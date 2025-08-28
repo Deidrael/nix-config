9d5cb78a688c53dd6215b9711999d1b11b7f7be9
Adam Grissom <14314946+Deidrael@users.noreply.github.com>
2025-08-28 02:35:26 -0500

    commenting out unused ./modules/home

--- home/adam/common/core/default.nix
+++ home/adam/common/core/default.nix
@@ -9,7 +9,7 @@
   imports = lib.flatten [
     (map lib.custom.relativeToRoot [
       "modules/common/host-spec.nix"
-      "modules/home"
+      #"modules/home"
     ])
     (lib.custom.scanPaths ./.)
   ];
