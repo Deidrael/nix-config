b3fb25aa0d584b866e192557be65c5ef7f6aad9a
Adam Grissom <14314946+Deidrael@users.noreply.github.com>
2025-09-30 21:19:30 -0500

    hostSpec wording cleanup

--- hosts/common/core/nixos.nix
+++ hosts/common/core/nixos.nix
@@ -29,7 +29,6 @@
   #
   # ========== Localization ==========
   #
-  i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
   time = {
     timeZone = lib.mkDefault "America/Chicago";
     hardwareClockInLocalTime = true;
--- modules/common/host-spec.nix
+++ modules/common/host-spec.nix
@@ -1,4 +1,4 @@
-# Specifications For Differentiating Hosts
+# Specifications For Differentiating Hosts - thanks to EmergentMind for module
 {
   config,
   pkgs,
@@ -41,6 +41,7 @@
         };
         domain = lib.mkOption {
           type = lib.types.str;
+          default = "local";
           description = "The domain of the host";
         };
         userFullName = lib.mkOption {
@@ -80,22 +81,22 @@
         isMinimal = lib.mkOption {
           type = lib.types.bool;
           default = false;
-          description = "Used to indicate a minimal host";
+          description = "Indicate a minimal host";
         };
         isProduction = lib.mkOption {
           type = lib.types.bool;
           default = true;
-          description = "Used to indicate a production host";
+          description = "Indicate a production host";
         };
         isServer = lib.mkOption {
           type = lib.types.bool;
           default = false;
-          description = "Used to indicate a server host";
+          description = "Indicate a server host";
         };
         isWork = lib.mkOption {
           type = lib.types.bool;
           default = false;
-          description = "Used to indicate a host that uses work resources";
+          description = "Indicate a host that uses work resources";
         };
         isDevelopment = lib.mkOption {
           type = lib.types.bool;
@@ -105,22 +106,22 @@
         isMobile = lib.mkOption {
           type = lib.types.bool;
           default = false;
-          description = "Used to indicate a mobile host";
+          description = "Indicate a mobile host";
         };
         useYubikey = lib.mkOption {
           type = lib.types.bool;
           default = false;
-          description = "Used to indicate if the host uses a yubikey";
+          description = "Indicate if the host uses a yubikey";
         };
         voiceCoding = lib.mkOption {
           type = lib.types.bool;
           default = false;
-          description = "Used to indicate a host that uses voice coding";
+          description = "Indicate a host that uses voice coding";
         };
         isAutoStyled = lib.mkOption {
           type = lib.types.bool;
           default = false;
-          description = "Used to indicate a host that wants auto styling like stylix";
+          description = "Indicate a host that wants auto styling like stylix";
         };
         theme = lib.mkOption {
           type = lib.types.str;
@@ -130,27 +131,27 @@
         useNeovimTerminal = lib.mkOption {
           type = lib.types.bool;
           default = false;
-          description = "Used to indicate a host that uses neovim for terminals";
+          description = "Indicate a host that uses neovim for terminals";
         };
         useWindowManager = lib.mkOption {
           type = lib.types.bool;
           default = true;
-          description = "Used to indicate a host that uses a window manager";
+          description = "Indicate a host that uses a window manager";
         };
         useAtticCache = lib.mkOption {
           type = lib.types.bool;
           default = true;
-          description = "Used to indicate a host that uses LAN atticd for caching";
+          description = "Indicate a host that uses LAN atticd for caching";
         };
         hdr = lib.mkOption {
           type = lib.types.bool;
           default = false;
-          description = "Used to indicate a host that uses HDR";
+          description = "Indicate a host that uses HDR";
         };
         scaling = lib.mkOption {
           type = lib.types.str;
           default = "1";
-          description = "Used to indicate what scaling to use. Floating point number";
+          description = "Indicate what scaling to use. Floating point number";
         };
         wallpaper = lib.mkOption {
           type = lib.types.path;
