99d3b1e8c14412a736f58c42bcc906c61f38d4c6
Adam Grissom <14314946+Deidrael@users.noreply.github.com>
2025-10-11 02:51:43 -0500

    fixing HP printer

--- modules/workstation/printing.nix
+++ modules/workstation/printing.nix
@@ -13,13 +13,15 @@
 
   # Adding my home printer
   hardware.printers = {
-    ensureDefaultPrinter = "HP_LaserJet_MFP_M234dw";
+    ensureDefaultPrinter = "HP_M234dw";
     ensurePrinters = [
       {
         name = "HP_M234dw";
         location = "Home";
         deviceUri = "ipp://10.69.3.50/ipp/print";
+        #model = "everywhere";
         model = "HP/hp-laserjet_mfp_m232-m237.ppd.gz";
+        ppdOptions.PageSize = "Letter";
       }
     ];
   };
