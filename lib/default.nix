9f2abbb4e932a7f5452aa6e429276b67a16242b1
Adam Grissom <14314946+Deidrael@users.noreply.github.com>
2025-10-27 21:57:16 -0500

    fixing recursivelyImport to not throw errors

--- lib/default.nix
+++ lib/default.nix
@@ -1,4 +1,3 @@
-# FIXME(lib.custom): Add some stuff from hmajid2301/dotfiles/lib/module/default.nix, as simplifies option declaration
 { lib, ... }:
 {
   # use path relative to the root of the project
@@ -20,20 +19,23 @@
       )
     );
 
-  # list all *.nix recursively
-  # let
-  #   inherit (lib) concatMap hasSuffix;
-  #   inherit (builtins) isPath filter readFileType;
+  # list all *.nix recursively (default.nix not required)
+  recursivelyImport =
+    list:
+    let
+      inherit (lib) concatMap hasSuffix;
+      inherit (builtins) isPath filter readFileType;
 
-  #   expandIfFolder = elem:
-  #     if !isPath elem || readFileType elem != "directory"
-  #       then [ elem ]
-  #     else lib.filesystem.listFilesRecursive elem;
-  # in
-  #   list: filter
-  #     # Filter out any path that doesn't look like `*.nix`. Don't forget to use
-  #     # toString to prevent copying paths to the store unnecessarily
-  #     (elem: !isPath elem || hasSuffix ".nix" (toString elem))
-  #     # Expand any folder to all the files within it.
-  #     (concatMap expandIfFolder list)
+      expandIfFolder =
+        elem:
+        if !isPath elem || readFileType elem != "directory" then
+          [ elem ]
+        else
+          lib.filesystem.listFilesRecursive elem;
+    in
+    filter
+      # Filter out any path that doesn't look like `*.nix`
+      (elem: !isPath elem || hasSuffix ".nix" (toString elem))
+      # Expand any folder to all the files within it
+      (concatMap expandIfFolder list);
 }
