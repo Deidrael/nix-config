d1a1e820390673277c0376297884bf83563ef785
Adam Grissom <14314946+Deidrael@users.noreply.github.com>
2025-11-05 02:53:24 -0600

    fixing from refactoring

--- hosts/nixos/inix/default.nix
+++ hosts/nixos/inix/default.nix
@@ -1,6 +1,6 @@
 {
-  config,
   inputs,
+  config,
   lib,
   ...
 }:
@@ -9,6 +9,7 @@
     # ========== Hardware ==========
     ./hardware-configuration.nix
     inputs.hardware.nixosModules.apple-imac-14-2
+    inputs.hardware.nixosModules.common-gpu-nvidia
 
     (lib.custom.relativeToRoot "modules/default.nix")
   ];
