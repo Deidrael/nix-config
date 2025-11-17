35c70d1c76684ee38fbf94754424a5f28aac6fbd
Adam Grissom <14314946+Deidrael@users.noreply.github.com>
2025-11-17 02:28:28 -0600

    Add CLI tools to user programs
    -Added copyq, fd, jq, ncdu, neofetch, ripgrep, and tree
     to the programs list in hosts/users/programs.nix for
     enhanced CLI functionality.

--- hosts/users/programs.nix
+++ hosts/users/programs.nix
@@ -13,25 +13,30 @@
     age
     btop # resource monitor
     cachix
+    copyq # clipboard manager
     coreutils # basic gnu utils
     curl
     delta
+    fd # tree style ls
     git
     gitui
     gh
     htop
+    jq # json pretty print
+    ncdu # TUI disk usage
+    neofetch # system info
     p7zip # compression & encryption
     pre-commit # git hooks
+    ripgrep # better grep
     sops
     ssh-to-age
+    #steam-run # for running non-NixOS-packaged binaries on Nix
     tmux
     #toybox
+    tree # cli dir tree viewer
     unzip # zip extraction
     vim-full
     wget # downloader
-
-    jq
-    #steam-run # for running non-NixOS-packaged binaries on Nix
     yq-go # yaml pretty printer and manipulator
     zip # zip compression
   ];
