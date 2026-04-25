b977012690d9aa901a06ed2e5d6fe2ba77f6ebab
Adam Grissom <14314946+Deidrael@users.noreply.github.com>
2026-04-25 12:46:55 -0500

    remove unused scanPaths from lib
    The scanPaths function is no longer used after switching to
    inputs.import-tree for recursive module imports.

--- lib/default.nix
+++ lib/default.nix
@@ -2,20 +2,4 @@
 {
   # use path relative to the root of the project
   relativeToRoot = lib.path.append ../.;
-
-  # list all *.nix and folders (requires default.nix inside)
-  scanPaths =
-    path:
-    builtins.map (f: (path + "/${f}")) (
-      builtins.attrNames (
-        lib.attrsets.filterAttrs (
-          path: _type:
-          (_type == "directory") # include directories
-          || (
-            (path != "default.nix") # ignore default.nix
-            && (lib.strings.hasSuffix ".nix" path) # include .nix files
-          )
-        ) (builtins.readDir path)
-      )
-    );
 }
