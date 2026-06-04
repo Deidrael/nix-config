0e5d6c1f437defa5ffa85f20abacd7d1ff2836d4
Adam Grissom <14314946+Deidrael@users.noreply.github.com>
2026-06-04 02:10:09 -0500

    Home Manager - specifying 26.05
    - master changed to 26.11 release, whereas nixos-unstable is still 26.05

--- flake.nix
+++ flake.nix
@@ -6,15 +6,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     # See also 'stable-packages' and 'unstable-packages' overlays at 'overlays/default.nix"
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-master.url = "github:NixOS/nixpkgs";
 
     hardware.url = "github:NixOS/nixos-hardware";
 
     home-manager = {
-      #url = "github:nix-community/home-manager/release-25.05";
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-26.05";
+      #url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
