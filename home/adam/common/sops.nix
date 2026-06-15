0b54e35004cbe71e7052ee90ea1c9dd8f1e30c04
Adam Grissom <14314946+Deidrael@users.noreply.github.com>
2026-06-15 16:32:54 -0500

    modules: rename modules/common to modules/base
    - Move modules/common/ to modules/base/ for clarity
    - Update path references in comments

--- home/adam/common/sops.nix
+++ home/adam/common/sops.nix
@@ -1,4 +1,4 @@
-# home level sops. see modules/common/sops.nix for hosts level
+# home level sops. see modules/base/services/sops.nix for hosts level
 {
   inputs,
   config,
@@ -12,7 +12,7 @@ in
   imports = [ inputs.sops-nix.homeManagerModules.sops ];
   sops = {
     # This is the location of the host specific age-key for primaryUser
-    # and will to have been extracted to this location via modules/common/sops.nix on the host
+    # and will to have been extracted to this location via modules/base/services/sops.nix on the host
     age.keyFile = "${homeDirectory}/.config/sops/age/keys.txt";
 
     defaultSopsFile = "${sopsFolder}/secrets.yaml";
