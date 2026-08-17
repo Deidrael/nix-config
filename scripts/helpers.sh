f35821883252cc9c813f04f56b7b8542544f6a1a
Adam Grissom <14314946+Deidrael@users.noreply.github.com>
2026-08-17 17:14:49 -0500

    sops: fix creation rules and test args
    - Fix age array in host creation rules to include all required keys
    - Remove leftover debug output from shared creation rules
    - Fix swapped test arguments to match function signature

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -88,8 +88,6 @@ function sops_add_shared_creation_rules() {
 
 	shared_selector='.creation_rules[] | select(.path_regex == "shared\.yaml$")'
 	if [[ -n $(yq "$shared_selector" "${SOPS_FILE}") ]]; then
-		echo "BEFORE"
-		cat "${SOPS_FILE}"
 		if [[ -z $(yq "$shared_selector.key_groups[].age[] | select(alias == $h)" "${SOPS_FILE}") ]]; then
 			green "Adding $u and $h to shared.yaml rule"
 			# NOTE: Split on purpose to avoid weird file corruption
@@ -113,7 +111,7 @@ function sops_add_host_creation_rules() {
 	host_selector=".creation_rules[] | select(.path_regex | contains(\"${host}\.yaml\"))"
 	if [[ -z $(yq "$host_selector" "${SOPS_FILE}") ]]; then
 		green "Adding new host file creation rule"
-		yq -i ".creation_rules += {\"path_regex\": \"${host}\\.yaml$\", \"key_groups\": [{\"age\": [$u, $h]}]}" "$SOPS_FILE"
+		yq -i ".creation_rules += {\"path_regex\": \"${host}\\.yaml$\", \"key_groups\": [{\"age\": [$u, $h, $w, $n]}]}" "$SOPS_FILE"
 		# Add aliases one by one
 		yq -i "($host_selector).key_groups[].age[0] alias = $u" "$SOPS_FILE"
 		yq -i "($host_selector).key_groups[].age[1] alias = $h" "$SOPS_FILE"
